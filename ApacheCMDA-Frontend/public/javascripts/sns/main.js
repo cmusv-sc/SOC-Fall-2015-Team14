e3d4d39ad44fa20ebaf1dfa3172cb955ef0ff34b
xunrongl <lixunrong@gmail.com>
2015-11-17 18:35:15 -0800

    add main page

---
+++ ApacheCMDA-Frontend/public/javascripts/sns/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lixunrong on 11/17/15.
+ */
+$(document).ready(function() {
+    //edit profile
+    $("#edit_btn").on("click", function() {
+        $(".profile-container").hide();
+        $(".profile-edit-container").show();
+    });
+
+})
